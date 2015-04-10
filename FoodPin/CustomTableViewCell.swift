5e809ea73dd152248130c47d447caddeb30fddd4
Anatoly Kovalev <inatoley@gmail.com>
2015-04-10 23:48:10 +0300

    yessssss
    do make

---
+++ FoodPin/CustomTableViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  CustomTableViewCell.swift
+//  FoodPin
+//
+//  Created by Fenkins on 10/04/15.
+//  Copyright (c) 2015 Fenkins. All rights reserved.
+//
+
+import UIKit
+
+class CustomTableViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
