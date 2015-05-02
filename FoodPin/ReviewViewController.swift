96780c394faca903075085e76fd86cc4bba1c7fb
Anatoly Kovalev <inatoley@gmail.com>
2015-05-02 19:02:53 +0300

    combining two types of animation

--- FoodPin/ReviewViewController.swift
+++ FoodPin/ReviewViewController.swift
@@ -20,10 +20,16 @@ class ReviewViewController: UIViewController {
         var blurEffectView = UIVisualEffectView(effect: blurEffect)
         blurEffectView.frame = view.bounds
         backgroundImageView.addSubview(blurEffectView)
-        // scaling
-        dialogView.transform = CGAffineTransformMakeScale(0.0, 0.0)
-        // Do any additional setup after loading the view.
+        // initial point for scaling or spring
+        // dialogView.transform = CGAffineTransformMakeScale(0.0, 0.0)
         
+        // initian point for sliding up
+        //dialogView.transform = CGAffineTransformMakeTranslation(0, 500)
+
+        // animation concatination
+        let scale = CGAffineTransformMakeScale(0.0, 0.0)
+        let translate = CGAffineTransformMakeTranslation(0, 500)
+        dialogView.transform = CGAffineTransformConcat(scale, translate)
     }
 
     override func viewDidAppear(animated: Bool) {
@@ -31,9 +37,22 @@ class ReviewViewController: UIViewController {
 //        UIView.animateWithDuration(0.2, delay: 0.0, options: nil, animations: {
 //            self.dialogView.transform = CGAffineTransformMakeScale(1.0, 1.0)
 //        }, completion: nil)
+        
         // spring animation
-        UIView.animateWithDuration(0.4, delay: 0.0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0.3, options: nil, animations: {
-            self.dialogView.transform = CGAffineTransformMakeScale(1.0, 1.0)
+//        UIView.animateWithDuration(0.4, delay: 0.0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0.3, options: nil, animations: {
+//            self.dialogView.transform = CGAffineTransformMakeScale(1.0, 1.0)
+//        }, completion: nil)
+        
+        // moving up
+//        UIView.animateWithDuration(0.7, delay: 0.0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0.5, options: nil, animations: {
+//            self.dialogView.transform = CGAffineTransformMakeTranslation(0.0, 0.0)
+//        }, completion: nil)
+
+        // animation concat
+        UIView.animateWithDuration(0.7, delay: 0.0, usingSpringWithDamping: 0.8, initialSpringVelocity: 0.5, options: nil, animations: {
+            let scale = CGAffineTransformMakeScale(1.0, 1.0)
+            let translate = CGAffineTransformMakeTranslation(0, 0)
+            self.dialogView.transform = CGAffineTransformConcat(scale, translate)
         }, completion: nil)
     }
     
