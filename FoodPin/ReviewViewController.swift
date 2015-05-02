b59d71a45d04211a7b3b302a96c0dba4fb5191fe
Anatoly Kovalev <inatoley@gmail.com>
2015-05-02 17:57:39 +0300

    review view subview now animated

--- FoodPin/ReviewViewController.swift
+++ FoodPin/ReviewViewController.swift
@@ -11,18 +11,27 @@ import UIKit
 class ReviewViewController: UIViewController {
 
     @IBOutlet weak var backgroundImageView: UIImageView!
+    @IBOutlet weak var dialogView: UIView!
     
     override func viewDidLoad() {
         super.viewDidLoad()
-        
+        //blur effect
         var blurEffect = UIBlurEffect(style: UIBlurEffectStyle.Dark)
         var blurEffectView = UIVisualEffectView(effect: blurEffect)
         blurEffectView.frame = view.bounds
         backgroundImageView.addSubview(blurEffectView)
+        //scaling
+        dialogView.transform = CGAffineTransformMakeScale(0.0, 0.0)
         // Do any additional setup after loading the view.
         
     }
 
+    override func viewDidAppear(animated: Bool) {
+        UIView.animateWithDuration(0.8, delay: 0.0, options: nil, animations: {
+            self.dialogView.transform = CGAffineTransformMakeScale(1.0, 1.0)
+        }, completion: nil)
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
