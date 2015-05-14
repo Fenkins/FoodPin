c5821db1c1cf85f86f5cd6556b017a46e422a6e4
Anatoly Kovalev <inatoley@gmail.com>
2015-05-14 17:42:19 +0300

    creating and connecting managed object

--- FoodPin/Restaurant.swift
+++ FoodPin/Restaurant.swift
@@ -7,22 +7,14 @@
 //
 
 import Foundation
+import CoreData
 
 
-class Restaurant {
-    var name:String = ""
-    var type:String = ""
-    var location:String = ""
-    var image:String = ""
-    var phone:String = ""
-    var isVisited:Bool = false
-    
-    init(name:String, type:String, location:String, image:String, phone:String, isVisited:Bool) {
-        self.name = name
-        self.type = type
-        self.location = location
-        self.image = image
-        self.phone = phone
-        self.isVisited = isVisited
-    }
+class Restaurant:NSManagedObject {
+    @NSManaged var name:String!
+    @NSManaged var type:String!
+    @NSManaged var location:String!
+    @NSManaged var image:NSData!
+    @NSManaged var phone:String!
+    @NSManaged var isVisited:NSNumber!
 }
