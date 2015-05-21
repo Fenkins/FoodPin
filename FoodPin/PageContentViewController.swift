06a6da4eb8c65e42ba42355115dea015790080db
Anatoly Kovalev <inatoley@gmail.com>
2015-05-21 21:01:02 +0300

    we'll continue tomorrow

---
+++ FoodPin/PageContentViewController.swift
@@ -0,0 +1,39 @@
+//
+//  PageContentViewController.swift
+//  FoodPin
+//
+//  Created by Fenkins on 21/05/15.
+//  Copyright (c) 2015 Fenkins. All rights reserved.
+//
+
+import UIKit
+
+class PageContentViewController: UIViewController {
+
+    @IBOutlet weak var headingLabel: UILabel!
+    @IBOutlet weak var subHeadingLabel: UILabel!
+    @IBOutlet weak var contentImageView: UIImageView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
