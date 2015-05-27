1d8e00b464561b3982223684260dbadfbea83429
Anatoly Kovalev <inatoley@gmail.com>
2015-05-27 23:14:30 +0300

    whatevs

---
+++ FoodPin/FeedDetailViewController.swift
@@ -0,0 +1,35 @@
+//
+//  FeedDetailViewController.swift
+//  FoodPin
+//
+//  Created by Fenkins on 27/05/15.
+//  Copyright (c) 2015 Fenkins. All rights reserved.
+//
+
+import UIKit
+
+class FeedDetailViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
